c460bfbd10cdc8c0d7fedd0c6436ff30ef7d7c10
Jayden Tan <jaydentan@mingBook-Pro.hsd1.ca.comcast.net>
2025-06-18 22:22:24 -0700

    minor visual tweaks

--- frontend/src/app/components/GuessGrid.tsx
+++ frontend/src/app/components/GuessGrid.tsx
@@ -4,18 +4,18 @@ export default function GuessGrid(){
     return(
         <div className="mx-auto max-w-3xl">
 
-            <div className="grid grid-cols-5 mb-2 text-center font-bold">
+            <div className="grid grid-cols-5 mb-2 text-center font-bold max-w-[98vw]">
                 {categories.map((name, i) => (
-                <h1 key={i} className="p-2">{name}</h1>
+                <h1 key={i} className="p-2 text-sm md:text-lg">{name}</h1>
                 ))}
             </div>
-
+            
             {Array.from({ length: 6 }).map((_, rowIndex) => (
-                <div key={rowIndex} className="grid grid-cols-5 gap-2 mb-2">
+                <div key={rowIndex} className="grid grid-cols-5 gap-2 mb-2 max-w-[98vw]">
                 {categories.map((_, colIndex) => (
                     <div 
                     key={colIndex} 
-                    className="border-2 border-gray-400 p-2 h-12 md:h-20 flex items-center justify-center duration-500"
+                    className="border-2 border-gray-400 rounded-s p-2 h-20 flex items-center justify-center duration-500"
                     >
                     </div>
                 ))}
@@ -24,8 +24,9 @@ export default function GuessGrid(){
 
             <div className="flex items-center justify-center">
                 <div className="grid">
-                    <input className="box border-2 border-gray-400 text-2xl m-1 duration-500 text-center w-2xl focus:outline-none" placeholder="Enter guess..."></input>
-                    <button className="box border-2 border-gray-400 text-2xl m-1 duration-500 text-center w-2xl hover:bg-white hover:text-[#121213]">SUBMIT</button>
+                    <div className="box border-2 border-gray-400 rounded-lg text-2xl m-1 duration-500 text-center w-2xl max-w-[98vw]">HINT</div>
+                    <input className="box border-2 border-gray-400 rounded-md text-2xl m-1 duration-500 text-center w-2xl focus:outline-none max-w-[98vw]" placeholder="Enter guess..."></input>
+                    <button className="box border-2 border-gray-400 rounded-md text-2xl font-bold m-1 duration-500 text-center w-2xl hover:bg-white hover:text-[#121213] max-w-[98vw]">SUBMIT</button>
                 </div>
             </div>
 
